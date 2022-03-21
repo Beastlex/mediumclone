652e6fc071145ff3a69681b4863bee43f1f9744e
beastlex <beastlex@gmail.com>
2022-03-21 02:09:36 +0000

    2022-03-21: add tag entity

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,6 +7,8 @@ const ormconfig: PostgresConnectionOptions = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
 };
 
 export default ormconfig;
